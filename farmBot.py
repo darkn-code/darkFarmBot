a40f614e090fc35186174433777653eebf4c4325
“Victor <ngironda7@gmail.com>
2021-03-04 12:13:03 -0600

    Frontend acabado

--- farmBot.py
+++ farmBot.py
@@ -10,6 +10,7 @@ border = "border: 0px solid black;"
 borderG = 'border:2px solid white;'
 letraTexto = QFont('SansSerif',10)
 letraTextoBold = QFont('SansSerif',10,QFont.Bold)
+letraTitulo = QFont('SansSerif',15,QFont.Bold)
 
 class MainWindow(QMainWindow):
 
@@ -25,21 +26,23 @@ class MainWindow(QMainWindow):
 
         Header = QFrame()
         Body = QFrame()
-        Footer = QFrame()
-
+        Footer = QGroupBox('Camara')
+        self.estiloLetraBlanco(Footer)
+        
+        gridLayout.setVerticalSpacing(0)
         gridLayout.addWidget(Header,0,0)
         gridLayout.addWidget(Body,1,0)
         gridLayout.addWidget(Footer,2,0)
 
-        HLayout = QGridLayout()
+        HLayout = QHBoxLayout()
         Header.setLayout(HLayout)
      
         fModoManual = QGroupBox('MODO MANUAL')
-        fModoManual.setFont(letraTexto)
+        fModoManual.setFont(letraTextoBold)
         fModoManual.setStyleSheet(ColorBotones)
         
         fModoAuto = QGroupBox('MODO AUTOMATICO')
-        fModoAuto.setFont(letraTexto)
+        fModoAuto.setFont(letraTextoBold)
         fModoAuto.setStyleSheet(ColorBotones)
         
         BodyLayout = QGridLayout()
@@ -48,67 +51,79 @@ class MainWindow(QMainWindow):
         BodyLayout.addWidget(fModoManual,0,0)
         BodyLayout.addWidget(fModoAuto,1,0)
 
-        FArduino = QFrame()
+        CLayout = QHBoxLayout()
+        Footer.setLayout(CLayout)
+        
+        #Modo manual
+        FEnviarDatos = QFrame()
         FControles = QFrame()
+        FDatos = QFrame()
         FBotones = QFrame()
 
+        DLayout = QVBoxLayout()
+        FDatos.setLayout(DLayout)
+
+        BLayout = QHBoxLayout()
+        FBotones.setLayout(BLayout)
+
         FALayout = QGridLayout()
-        FArduino.setLayout(FALayout)
+        FEnviarDatos.setLayout(FALayout)
 
         Mlayout = QGridLayout()
         fModoManual.setLayout(Mlayout)
         
-        Mlayout.addWidget(FArduino,0,0)
-        Mlayout.addWidget(FControles,1,0)
-        Mlayout.addWidget(FBotones,2,0)
+        Mlayout.setVerticalSpacing(0)
+        Mlayout.addWidget(FEnviarDatos,2,0)
+        Mlayout.addWidget(FControles,0,0)
+        Mlayout.addWidget(FDatos,1,0)
+        Mlayout.addWidget(FBotones,3,0)
 
         LFrame = QGroupBox("XY")
-        LFrame.setFont(letraTexto)
-        LFrame.setStyleSheet(ColorBotones)
+        self.estiloLetraBlanco(LFrame)
         MFrame = QGroupBox("Z")
-        MFrame.setFont(letraTexto)
-        MFrame.setStyleSheet(ColorBotones)
-        RFrame = QGroupBox("Enviar datos")
-        RFrame.setFont(letraTexto)
-        RFrame.setStyleSheet(ColorBotones)
+        self.estiloLetraBlanco(MFrame)
+        RFrame = QGroupBox("Arduino")
+        self.estiloLetraBlanco(RFrame)
 
         FLayout = QGridLayout()
         FControles.setLayout(FLayout)
 
-        FLayout.addWidget(LFrame,0,0)
-        FLayout.addWidget(MFrame,0,1)
-        FLayout.addWidget(RFrame,0,2)
+        FLayout.addWidget(LFrame,0,1)
+        FLayout.addWidget(MFrame,0,2)
+        FLayout.addWidget(RFrame,0,0)
 
         ZLayout = QGridLayout()
         MFrame.setLayout(ZLayout)
         
-        EnvDatos = QGridLayout()
-        RFrame.setLayout(EnvDatos)
+        ArLayout = QGridLayout()
+        RFrame.setLayout(ArLayout)
 
         layout = QGridLayout()
         LFrame.setLayout(layout)
 
+        #modo automatico
+        ALayout = QHBoxLayout()
+        fModoAuto.setLayout(ALayout)
+
         #Imagen cicata
         cicata = QLabel(self)
         pixmap2 = QPixmap('./pictures/cicata.png')
-        cicata.setStyleSheet(border)
         cicata.setPixmap(pixmap2)
+        #cicata.setAlignment(Qt.AlignCenter)
 
         #titulo Header
         titulo = QLabel()
         titulo.setText("FarmBot Chile Habanero")
-        titulo.setFont(QFont('SansSerif',15))
-        titulo.setStyleSheet(colorLetra + border)
+        titulo.setFont(letraTitulo)
+        titulo.setStyleSheet(colorLetra)
         
         #Modo manual
         bConectar = QPushButton("Conectar")
-        bConectar.setStyleSheet(ColorBotones)
-        bConectar.setFont(letraTexto)
         bConectar.clicked.connect(self.conectarArduino)
+        self.estiloLetraBlanco(bConectar)
 
         tPuerto = QLineEdit("/dev/ttyACM0")
-        tPuerto.setStyleSheet(ColorBotones)
-        tPuerto.setFont(letraTexto)
+        self.estiloLetraBlanco(tPuerto)
 
         bArriba = QPushButton()
         bArriba.setIcon(QIcon('./pictures/flechaArr.png'))
@@ -124,13 +139,39 @@ class MainWindow(QMainWindow):
         bAbajoZ = QPushButton()
         bAbajoZ.setIcon(QIcon('./pictures/flechaAba.png'))
 
+        bLuz = QPushButton('Luz')
+        self.estiloLetraBlanco(bLuz)
+        
+        bAgua = QPushButton('Agua')
+        self.estiloLetraBlanco(bAgua)
+         
+        bVacio = QPushButton('Bomba Vacio')
+        self.estiloLetraBlanco(bVacio)
+      
         bEnviar = QPushButton('Enviar')
-        bEnviar.setStyleSheet(ColorBotones)
-        bEnviar.setFont(letraTexto)
+        self.estiloLetraBlanco(bEnviar)
+
+        bRegar = QPushButton('Regar')
+        self.estiloLetraBlanco(bRegar)
+
+        bSembrar = QPushButton('Sembrar')
+        self.estiloLetraBlanco(bSembrar)
+
+        bHumedad = QPushButton('Humedad')
+        self.estiloLetraBlanco(bHumedad)
+        
+        bCamara = QPushButton('Camara')
+        self.estiloLetraBlanco(bCamara)
+
+        bAi = QPushButton('AI')
+        self.estiloLetraBlanco(bAi)
+        
+        tData = QLineEdit('Data del Arduino')
+        tData.setReadOnly(True)
+        self.estiloLetraBlanco(tData)
 
         tEnviar = QLineEdit()
-        tEnviar.setStyleSheet(ColorBotones)
-        tEnviar.setFont(letraTexto)
+        self.estiloLetraBlanco(tEnviar)
         
 
         #shortcut
@@ -138,40 +179,52 @@ class MainWindow(QMainWindow):
         sConectar.activated.connect(self.conectarArduino)
             
         #LAYOUUT
-        HLayout.addWidget(cicata,0,0)
-        HLayout.addWidget(titulo,0,1)
-
-        FALayout.addWidget(bConectar,0,0)
-        FALayout.addWidget(tPuerto,0,1)
+        HLayout.addWidget(cicata)
+        HLayout.addWidget(titulo)
         
+        ArLayout.addWidget(tPuerto,0,0)
+        ArLayout.addWidget(bConectar,1,0)
+
+        layout.setVerticalSpacing(0)
+        layout.setHorizontalSpacing(0)
         layout.addWidget(bArriba,0,1)
         layout.addWidget(bAbajo,1,1)
         layout.addWidget(bDerecha,1,2)
         layout.addWidget(bIzquierda,1,0)
-
+        
+        ZLayout.setVerticalSpacing(0)
         ZLayout.addWidget(bArribaZ,0,0)
         ZLayout.addWidget(bAbajoZ,1,0)
 
-        EnvDatos.addWidget(tEnviar,0,0)
-        EnvDatos.addWidget(bEnviar,1,0)
-         
+        DLayout.addWidget(tData)
+        
+        FALayout.addWidget(bEnviar,0,1)
+        FALayout.addWidget(tEnviar,0,0)
+        
+        BLayout.addWidget(bLuz)
+        BLayout.addWidget(bAgua)
+        BLayout.addWidget(bVacio)
+        
+        ALayout.addWidget(bRegar)
+        ALayout.addWidget(bSembrar)
+        ALayout.addWidget(bHumedad)
+
+        CLayout.addWidget(bCamara)
+        CLayout.addWidget(bAi)
+
+
         win.setFixedSize(gridLayout.sizeHint())
         self.setFixedSize(gridLayout.sizeHint()) 
 
-    def conectarArduino(self):
-        msgBox = QMessageBox()
-        msgBox.setIcon(QMessageBox.Information)
-        msgBox.setText("conectando")
-        msgBox.setWindowTitle("QMessageBox Example")
-        msgBox.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
-        msgBox.buttonClicked.connect(self.msgButtonClick)
-        returnValue = msgBox.exec()
 
-        if returnValue == QMessageBox.Ok:
-            print('OK clicked')
-                         
-    def msgButtonClick(self,i):
-        print("Button clicked is:",i.text())
+    def estiloLetraBlanco(self,widget):
+        widget.setStyleSheet(ColorBotones)
+        widget.setFont(letraTexto)
+
+
+    def conectarArduino(self):
+        print('Hola')
+                    
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
