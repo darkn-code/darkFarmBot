43e25e78b2d65832dba7a8aa80b00cfa54e9c883
“Victor <ngironda7@gmail.com>
2021-03-03 23:35:45 -0600

    Usando grid y frame

--- farmBot.py
+++ farmBot.py
@@ -8,6 +8,8 @@ colorLetra = "color:#419f00;"
 ColorBotones = "color:white;"
 border = "border: 0px solid black;"
 borderG = 'border:2px solid white;'
+letraTexto = QFont('SansSerif',10)
+letraTextoBold = QFont('SansSerif',10,QFont.Bold)
 
 class MainWindow(QMainWindow):
 
@@ -17,15 +19,74 @@ class MainWindow(QMainWindow):
         self.setStyleSheet(fondo)
 
         #Creacion Widget
-        WFrame = QWidget(self)
+        win = QWidget(self)
+        gridLayout = QGridLayout()
+        win.setLayout(gridLayout)
+
+        Header = QFrame()
+        Body = QFrame()
+        Footer = QFrame()
+
+        gridLayout.addWidget(Header,0,0)
+        gridLayout.addWidget(Body,1,0)
+        gridLayout.addWidget(Footer,2,0)
+
+        HLayout = QGridLayout()
+        Header.setLayout(HLayout)
+     
+        fModoManual = QGroupBox('MODO MANUAL')
+        fModoManual.setFont(letraTexto)
+        fModoManual.setStyleSheet(ColorBotones)
+        
+        fModoAuto = QGroupBox('MODO AUTOMATICO')
+        fModoAuto.setFont(letraTexto)
+        fModoAuto.setStyleSheet(ColorBotones)
+        
+        BodyLayout = QGridLayout()
+        Body.setLayout(BodyLayout)
+
+        BodyLayout.addWidget(fModoManual,0,0)
+        BodyLayout.addWidget(fModoAuto,1,0)
+
+        FArduino = QFrame()
+        FControles = QFrame()
+        FBotones = QFrame()
+
+        FALayout = QGridLayout()
+        FArduino.setLayout(FALayout)
+
+        Mlayout = QGridLayout()
+        fModoManual.setLayout(Mlayout)
+        
+        Mlayout.addWidget(FArduino,0,0)
+        Mlayout.addWidget(FControles,1,0)
+        Mlayout.addWidget(FBotones,2,0)
+
+        LFrame = QGroupBox("XY")
+        LFrame.setFont(letraTexto)
+        LFrame.setStyleSheet(ColorBotones)
+        MFrame = QGroupBox("Z")
+        MFrame.setFont(letraTexto)
+        MFrame.setStyleSheet(ColorBotones)
+        RFrame = QGroupBox("Enviar datos")
+        RFrame.setFont(letraTexto)
+        RFrame.setStyleSheet(ColorBotones)
+
+        FLayout = QGridLayout()
+        FControles.setLayout(FLayout)
+
+        FLayout.addWidget(LFrame,0,0)
+        FLayout.addWidget(MFrame,0,1)
+        FLayout.addWidget(RFrame,0,2)
+
+        ZLayout = QGridLayout()
+        MFrame.setLayout(ZLayout)
+        
+        EnvDatos = QGridLayout()
+        RFrame.setLayout(EnvDatos)
+
         layout = QGridLayout()
-        BFrame = QWidget()
-        ZFrame = QWidget()
-        blayout = QGridLayout()
-        zlayout = QGridLayout()
-        ZFrame.setLayout(zlayout)
-        BFrame.setLayout(blayout)
-        WFrame.setLayout(layout)
+        LFrame.setLayout(layout)
 
         #Imagen cicata
         cicata = QLabel(self)
@@ -38,26 +99,16 @@ class MainWindow(QMainWindow):
         titulo.setText("FarmBot Chile Habanero")
         titulo.setFont(QFont('SansSerif',15))
         titulo.setStyleSheet(colorLetra + border)
-
-        lXY = QLabel()
-        lXY.setText('XY')
-        lXY.setStyleSheet(ColorBotones)
-        lXY.setFont(QFont('SansSerif',10))
-        
-        lz = QLabel()
-        lz.setText('Z')
-        lz.setStyleSheet(ColorBotones)
-        lz.setFont(QFont('SansSerif',10))
         
         #Modo manual
         bConectar = QPushButton("Conectar")
         bConectar.setStyleSheet(ColorBotones)
-        bConectar.setFont(QFont("SansSerif",10))
+        bConectar.setFont(letraTexto)
         bConectar.clicked.connect(self.conectarArduino)
 
         tPuerto = QLineEdit("/dev/ttyACM0")
         tPuerto.setStyleSheet(ColorBotones)
-        tPuerto.setFont(QFont('SansSerif',10))
+        tPuerto.setFont(letraTexto)
 
         bArriba = QPushButton()
         bArriba.setIcon(QIcon('./pictures/flechaArr.png'))
@@ -75,37 +126,37 @@ class MainWindow(QMainWindow):
 
         bEnviar = QPushButton('Enviar')
         bEnviar.setStyleSheet(ColorBotones)
-        bEnviar.setFont(QFont('SansSerif',10))
+        bEnviar.setFont(letraTexto)
 
         tEnviar = QLineEdit()
         tEnviar.setStyleSheet(ColorBotones)
-        tEnviar.setFont(QFont('SansSerif',10))
+        tEnviar.setFont(letraTexto)
         
 
         #shortcut
-        sConectar = QShortcut(QKeySequence("Enter"),WFrame)
+        sConectar = QShortcut(QKeySequence("Enter"),win)
         sConectar.activated.connect(self.conectarArduino)
             
         #LAYOUUT
-        layout.addWidget(titulo,0,1)
-        layout.addWidget(cicata,0,0)
-        layout.addWidget(bConectar,1,0)
-        layout.addWidget(tPuerto,1,1)
-        layout.addWidget(BFrame,3,0)
-        layout.addWidget(ZFrame,3,1)
-        blayout.addWidget(bArriba,0,1)
-        blayout.addWidget(bAbajo,1,1)
-        blayout.addWidget(bDerecha,1,2)
-        blayout.addWidget(bIzquierda,1,0)
-        blayout.addWidget(lXY,0,0)
-        zlayout.addWidget(lz,0,0)
-        zlayout.addWidget(bArribaZ,0,1)
-        zlayout.addWidget(bAbajoZ,1,1)
-        zlayout.addWidget(tEnviar,0,2)
-        zlayout.addWidget(bEnviar,1,2)
+        HLayout.addWidget(cicata,0,0)
+        HLayout.addWidget(titulo,0,1)
+
+        FALayout.addWidget(bConectar,0,0)
+        FALayout.addWidget(tPuerto,0,1)
+        
+        layout.addWidget(bArriba,0,1)
+        layout.addWidget(bAbajo,1,1)
+        layout.addWidget(bDerecha,1,2)
+        layout.addWidget(bIzquierda,1,0)
+
+        ZLayout.addWidget(bArribaZ,0,0)
+        ZLayout.addWidget(bAbajoZ,1,0)
+
+        EnvDatos.addWidget(tEnviar,0,0)
+        EnvDatos.addWidget(bEnviar,1,0)
          
-        WFrame.setFixedSize(layout.sizeHint()) 
-        self.setFixedSize(layout.sizeHint()) 
+        win.setFixedSize(gridLayout.sizeHint())
+        self.setFixedSize(gridLayout.sizeHint()) 
 
     def conectarArduino(self):
         msgBox = QMessageBox()
